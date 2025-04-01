7bf1ef83b99244fcfc33c1d4d67fc8a65773763b
marcopiii <marpi93@icloud.com>
2025-04-01 16:42:49 +0200

    rework camera positions definitions

--- js/CameraManager.js
+++ js/CameraManager.js
@@ -8,36 +8,44 @@ export class CameraManager {
     #frustumSize = 25;
 
     #config = {
-        "x-plane": {
-            position: new Vector3( -10, 0, 0.5 * BLOCK_SIZE),
-            lookAt: new Vector3(0,0,0.5 * BLOCK_SIZE),
-        },
-        "z-plane": {
-            position: new Vector3(0.5 * BLOCK_SIZE, 0, 10),
-            lookAt: new Vector3(0.5 * BLOCK_SIZE, 0, 0)
-        },
-        "isometric_X+Z+": {
+        "xR_zR": {
             position: new Vector3(-10, 5, 10 + BLOCK_SIZE),
             lookAt: new Vector3(0, 0, BLOCK_SIZE)
         },
-        "isometric_X-Z+": {
+        "xL_zR": {
             position: new Vector3(10, 5, 10 + BLOCK_SIZE),
             lookAt: new Vector3(0, 0, BLOCK_SIZE)
         },
-        "isometric_X-Z-": {
+        "xL_zL": {
             position: new Vector3(10, 5, -10 - BLOCK_SIZE),
             lookAt: new Vector3(0, 0, -BLOCK_SIZE)
         },
-        "isometric_X+Z-": {
+        "xR_zL": {
             position: new Vector3(-10, 5, -10 - BLOCK_SIZE),
             lookAt: new Vector3(0, 0, -BLOCK_SIZE)
         },
+        "x0_zR": {
+            position: new Vector3( -10, 0, 0.5 * BLOCK_SIZE),
+            lookAt: new Vector3(0,0,0.5 * BLOCK_SIZE),
+        },
+        "x0_zL": {
+            position: new Vector3( 10, 0, 0.5 * BLOCK_SIZE),
+            lookAt: new Vector3(0,0,0.5 * BLOCK_SIZE),
+        },
+        "xR_z0": {
+            position: new Vector3(0.5 * BLOCK_SIZE, 0, 10),
+            lookAt: new Vector3(0.5 * BLOCK_SIZE, 0, 0)
+        },
+        "xL_z0": {
+            position: new Vector3(0.5 * BLOCK_SIZE, 0, -10),
+            lookAt: new Vector3(0.5 * BLOCK_SIZE, 0, 0)
+        }
     }
 
     constructor(container: HTMLElement) {
         const aspect = container.clientWidth / container.clientHeight
         this._tweenGroup = new TWEEN.Group();
-        this._position = "isometric_X+Z+"
+        this._position = "xR_zR"
         this._camera = new THREE.OrthographicCamera(
             - this.#frustumSize * aspect / 2,
             this.#frustumSize * aspect / 2,
@@ -59,17 +67,17 @@ export class CameraManager {
 
     move(direction: "right" | "left") {
         switch (this._position) {
-            case "isometric_X+Z+":
-                this._position = direction === "right" ? "isometric_X-Z+" : "isometric_X+Z-";
+            case "xR_zR":
+                this._position = direction === "right" ? "xL_zR" : "xR_zL";
                 break;
-            case "isometric_X-Z+":
-                this._position = direction === "right" ? "isometric_X-Z-" : "isometric_X+Z+";
+            case "xL_zR":
+                this._position = direction === "right" ? "xL_zL" : "xR_zR";
                 break;
-            case "isometric_X-Z-":
-                this._position = direction === "right" ? "isometric_X+Z-" : "isometric_X-Z+";
+            case "xL_zL":
+                this._position = direction === "right" ? "xR_zL" : "xL_zR";
                 break;
-            case "isometric_X+Z-":
-                this._position = direction === "right" ? "isometric_X+Z+" : "isometric_X-Z-";
+            case "xR_zL":
+                this._position = direction === "right" ? "xR_zR" : "xL_zL";
                 break;
         }
         const target = this.#config[this._position];
