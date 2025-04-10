6bc35943498bb5a615c11d7edabb861023cf43af
marcopiii <marpi93@icloud.com>
2025-04-10 22:35:04 +0200

    setup prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,20 +2,22 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const { resolve } = require("node:path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './src/index.html',
     }),
+    new MiniCssExtractPlugin(),
     new CopyPlugin({
       patterns: [
-        { from: 'css', to: 'css' },
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'icon.png', to: 'icon.png' },
