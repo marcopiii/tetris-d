28d8cb3b1c44e422d7361e3ab2c9126f1b250762
marcopiii <marpi93@icloud.com>
2025-04-12 18:17:31 +0200

    change the speed based on level

--- src/scenario/pvpGame/Clock.ts
+++ src/scenario/pvpGame/Clock.ts
@@ -1,12 +1,16 @@
 export class Clock {
   private readonly _callback: () => void;
   private _interval: NodeJS.Timeout | undefined;
-
-  private SPEED = 1500;
+  private _level: number;
 
   constructor(callback: () => void) {
+    this._level = 1;
     this._callback = callback;
     this._interval = undefined;
+  }
+
+  private speed() {
+    return 1 / (gravity[this._level] * 60);
   }
 
   get isRunning(): boolean {
@@ -18,12 +22,23 @@ export class Clock {
       clearInterval(this._interval);
       this._interval = undefined;
     } else {
-      this._interval = setInterval(this._callback, this.SPEED);
+      this._interval = setInterval(this._callback, this.speed());
     }
   }
 
   start() {
     clearInterval(this._interval);
-    this._interval = setInterval(this._callback, this.SPEED);
+    this._interval = setInterval(this._callback, this.speed());
+  }
+
+  set level(lvl: number) {
+    this._level = lvl;
+    clearInterval(this._interval);
+    this._interval = setInterval(this._callback, this.speed());
   }
 }
+
+const gravity = [
+  0, 0.01667, 0.021017, 0.026977, 0.035256, 0.04693, 0.06361, 0.0879, 0.1236,
+  0.1775, 0.2598, 0.388, 0.59, 0.92, 1.46, 2.36,
+];
