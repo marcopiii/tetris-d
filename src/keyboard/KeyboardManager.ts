bb9c1a5d32fd4202a6d01c543db111119d9e10d8
marcopiii <marpi93@icloud.com>
2025-04-18 15:00:01 +0200

    implement KeyboardManager

---
+++ src/keyboard/KeyboardManager.ts
@@ -0,0 +1,28 @@
+import { Event, Button } from '../gamepad/types';
+
+export class KeyboardManager {
+  private _handler: (event: Event, button: KeyboardEvent['code']) => void;
+
+  constructor() {
+    this._handler = () => {};
+
+    this._handleKeyDown = this._handleKeyDown;
+    this._handleKeyUp = this._handleKeyUp;
+
+    window.addEventListener('keydown', this._handleKeyDown);
+    window.addEventListener('keyup', this._handleKeyUp);
+  }
+
+  set handler(handler: (event: Event, button: KeyboardEvent['code']) => void) {
+    this._handler = handler;
+  }
+
+  private _handleKeyDown = (event: KeyboardEvent) => {
+    // if (event.repeat) return;
+    this._handler('press', event.code);
+  };
+
+  private _handleKeyUp = (event: KeyboardEvent) => {
+    this._handler('release', event.code);
+  };
+}
