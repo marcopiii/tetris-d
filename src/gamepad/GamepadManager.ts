2a77415b84ffadd38bfdc4729233124277e56c19
marcopiii <marpi93@icloud.com>
2025-04-24 18:20:08 +0200

    implement button hold support

--- src/gamepad/GamepadManager.ts
+++ src/gamepad/GamepadManager.ts
@@ -16,18 +16,21 @@ export type Button =
   | 'LT'
   | 'RT';
 
-export type Event = 'press' | 'release';
+export type Event = 'press' | 'hold' | 'release' | 'lift';
+
+const HOLD_FRAMES = 7;
+const BUFFER_SIZE = HOLD_FRAMES + 1;
 
 export class GamepadManager {
   private _handler: (event: Event, button: Button) => void;
   private readonly _gamepadIndex: number;
-  private _buffer: readonly GamepadButton[];
+  private _buffer: readonly GamepadButton[][];
   private _active: boolean;
 
   constructor(index: number) {
     this._handler = () => {};
     this._gamepadIndex = index;
-    this._buffer = [];
+    this._buffer = new Array(BUFFER_SIZE).fill([]);
     this._active = index === 0;
   }
 
@@ -36,7 +39,7 @@ export class GamepadManager {
   }
 
   set active(active: boolean) {
-    this._buffer = [];
+    this._buffer = new Array(BUFFER_SIZE).fill([]);
     this._active = active;
   }
 
@@ -48,11 +51,23 @@ export class GamepadManager {
     gamepad.buttons.forEach((button, i) => {
       const buttonCode = mapping(i);
       if (!buttonCode) return;
-      if (button.pressed && !this._buffer[i]?.pressed)
+
+      const wasPressed = this._buffer[0][i]?.pressed ?? false;
+      const isHolding = this._buffer.slice(0,HOLD_FRAMES).every((b) => b[i]?.pressed ?? false);
+      const wasHeld = this._buffer.every((b) => b[i]?.pressed ?? false)
+
+      if (button.pressed && !wasPressed)
         this._handler('press', buttonCode);
-      if (!button.pressed && this._buffer[i]?.pressed)
+      if (button.pressed && isHolding && !wasHeld)
+        this._handler('hold', buttonCode);
+      if (!button.pressed && wasPressed && !wasHeld)
+        this._handler('lift', buttonCode);
+      if (!button.pressed && wasHeld)
         this._handler('release', buttonCode);
     });
-    this._buffer = gamepad.buttons;
+    this._buffer = [[...gamepad.buttons], ...this._buffer].slice(
+      0,
+      BUFFER_SIZE,
+    );
   }
 }
