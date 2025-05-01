6a45cfea482a7ebd1be0469feedce50f5a71421a
marcopiii <marpi93@icloud.com>
2025-05-01 22:42:03 +0200

    define plane multiplier

--- src/scenario/game/Progress.ts
+++ src/scenario/game/Progress.ts
@@ -25,7 +25,27 @@ export class Progress {
   }
 }
 
+export type LineCoord = { y: number } & (
+  | { x: number; z?: never }
+  | { x?: never; z: number }
+  );
+
 function scorePerLines(n: number) {
   if (n < 1) return 0;
   return 50 * n ** 2 + 50 * n;
+}
+
+function planeMultiplier(lines: LineCoord[]) {
+  if (lines.length < 2) return 1;
+
+  const orthogonalPlanes =
+    lines.some((line) => 'x' in line) && lines.some((line) => 'z' in line);
+  if (orthogonalPlanes) return 2;
+
+  const parallelPlanes =
+    lines.some((line) => line.x != lines[0].x) ||
+    lines.some((line) => line.z != lines[0].z);
+  if (parallelPlanes) return 1.5;
+
+  return 1;
 }
