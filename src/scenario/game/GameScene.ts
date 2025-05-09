934955739fc4278dddb34dbfc63a63f1d181aa27
marcopiii <marpi93@icloud.com>
2025-05-10 00:28:49 +0200

    add fog to the scene

--- src/scenario/game/GameScene.ts
+++ src/scenario/game/GameScene.ts
@@ -30,6 +30,8 @@ export abstract class GameScene {
     this._scene = scene;
     this._scene.clear();
 
+    this._scene.fog = new THREE.Fog("black", 10, 40)
+
     this._tetrion = new THREE.Group();
     this._board = new THREE.Group();
     this._piece = new THREE.Group();
