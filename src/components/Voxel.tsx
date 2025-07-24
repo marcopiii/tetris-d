324904d0644ea0b4fb6bee8e751c5ef06002cd0c
marcopiii <marpi93@icloud.com>
2025-07-24 20:46:45 +0200

    implement Voxel component

---
+++ src/components/Voxel.tsx
@@ -0,0 +1,32 @@
+import * as THREE from 'three';
+
+type Props = {
+  position: THREE.Vector3Like;
+  size: number;
+  material:
+    | [THREE.MeshBasicMaterialParameters]
+    | [
+        THREE.MeshBasicMaterialParameters,
+        THREE.MeshBasicMaterialParameters,
+        THREE.MeshBasicMaterialParameters,
+      ];
+};
+
+export default function Voxel(props: Props) {
+  const position = new THREE.Vector3(
+    props.position.x,
+    props.position.y,
+    props.position.z,
+  );
+
+  const size = [props.size, props.size, props.size] as const;
+
+  return (
+    <mesh position={position}>
+      <boxGeometry args={size} />
+      {props.material.map((materialParams) => (
+        <meshBasicMaterial {...materialParams} />
+      ))}
+    </mesh>
+  );
+}
