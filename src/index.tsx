1f39124191759981ce7154588706eb2869b46434
marcopiii <marpi93@icloud.com>
2025-07-27 14:37:03 +0200

    setup camera

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,35 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
+import { MainMenu } from './MainMenu';
 import './style.css';
 
-function App() {
+function App(props: { aspectRatio: number }) {
+  const frustumHeight = 22;
+
+  const frustumProps = {
+    left: (-frustumHeight * props.aspectRatio) / 2,
+    right: (frustumHeight * props.aspectRatio) / 2,
+    top: frustumHeight / 2,
+    bottom: -frustumHeight / 2,
+    near: 0.1,
+    far: 1000,
+    frustumCulled: true,
+  };
+
   return (
-    <div id="canvas-container">
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <octahedronGeometry args={[2, 3]} />
-          <meshBasicMaterial />
-        </mesh>
-      </Canvas>
-    </div>
+    <Canvas orthographic camera={frustumProps}>
+      <axesHelper args={[10]} />
+      <MainMenu
+        onPvE={() => console.log('play')}
+        onControls={() => console.log('controls')}
+        onAbout={() => console.log('about')}
+      />
+    </Canvas>
   );
 }
 
-createRoot(document.getElementById('root')!).render(<App />);
+const root = document.getElementById('root')!;
+createRoot(root).render(
+  <App aspectRatio={root.clientWidth / root.clientHeight} />,
+);
