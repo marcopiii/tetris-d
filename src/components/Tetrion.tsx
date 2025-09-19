afefa3e10752bb3b99c182489260e77a50139fe4
marcopiii <marpi93@icloud.com>
2025-09-19 13:06:52 +0200

    tone down fog

--- src/components/Tetrion.tsx
+++ src/components/Tetrion.tsx
@@ -13,7 +13,7 @@ export default function Tetrion() {
   const scene = useThree((rootState) => rootState.scene);
 
   React.useEffect(() => {
-    scene.fog = new THREE.Fog('black', 10, 40);
+    scene.fog = new THREE.Fog('black', 10, 70);
     scene.background = new THREE.Color('#b5c5d2');
     return () => {
       scene.fog = null;
