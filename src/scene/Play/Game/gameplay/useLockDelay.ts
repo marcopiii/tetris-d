cf53d7c91e555e4d0569a2c18549896a42548795
marcopiii <marpi93@icloud.com>
2025-10-22 22:09:14 +0200

    use ref instead of state

--- src/scene/Play/Game/gameplay/useLockDelay.ts
+++ src/scene/Play/Game/gameplay/useLockDelay.ts
@@ -4,33 +4,32 @@ const LOCK_DELAY_MS = 500;
 const MAX_LOCK_DELAY_RESETS = 15;
 
 export default function useLockDelay(callback: () => void) {
-  const [timerId, setTimerId] = React.useState<NodeJS.Timeout>();
-  const [resetCounter, setResetCounter] = React.useState(0);
+  const timerId = React.useRef<number>(undefined);
+  const resetCounterRef = React.useRef(0);
 
   const fn = () => {
     callback();
-    setTimerId(undefined);
-    setResetCounter(0);
+    timerId.current = undefined;
+    resetCounterRef.current = 0;
   };
 
   const triggerLock = () => {
-    if (timerId) {
-      clearTimeout(timerId);
-      setResetCounter((count) => count + 1);
+    if (timerId.current) {
+      clearTimeout(timerId.current);
+      resetCounterRef.current += 1;
     }
-    const newTimerId = setTimeout(fn, LOCK_DELAY_MS);
-    setTimerId(newTimerId);
+    timerId.current = window.setTimeout(fn, LOCK_DELAY_MS);
   };
 
   const cancelLock = () => {
-    if (timerId) {
-      clearTimeout(timerId);
-      setTimerId(undefined);
-      setResetCounter(0);
+    if (timerId.current) {
+      clearTimeout(timerId.current);
+      timerId.current = undefined;
+      resetCounterRef.current = 0;
     }
   };
 
-  const locked = resetCounter >= MAX_LOCK_DELAY_RESETS;
+  const canReset = resetCounterRef.current >= MAX_LOCK_DELAY_RESETS;
 
-  return [triggerLock, cancelLock, locked] as const;
+  return [triggerLock, cancelLock, canReset] as const;
 }
