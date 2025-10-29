909672adbffcb24b6397a7c60cbcabb1475c2df5
marcopiii <marpi93@icloud.com>
2025-10-29 13:36:46 +0100

    mino refactor

--- src/scene/Play/Game/gameplay/cutter/types.ts
+++ src/scene/Play/Game/gameplay/cutter/types.ts
@@ -1,3 +1,4 @@
 export type AbsoluteSide = 'below' | 'above';
-
 export type RelativeSide = 'left' | 'right';
+export type AbsoluteCut = Record<AbsoluteSide, boolean>;
+export type RelativeCut = Record<RelativeSide, boolean>;
