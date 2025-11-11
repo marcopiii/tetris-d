4eba47b170bc2b177467ced8c747ed8327cf521e
marcopiii <marpi93@icloud.com>
2025-11-11 19:40:34 +0100

    detect full zic

--- src/scene/Play/Game/gameplay/score/zicDetector.ts
+++ src/scene/Play/Game/gameplay/score/zicDetector.ts
@@ -1,5 +1,7 @@
+import { match } from 'ts-pattern';
 import { calculateMatrix } from '~/scene/Play/Game/gameplay/calculateMatrix';
 import { detectCollision } from '~/scene/Play/Game/gameplay/detectCollision';
+import { COLS, ROWS, VANISH_ZONE_ROWS } from '~/scene/Play/Game/params';
 import { ZicData } from './TrackEvent';
 import { shiftLeft, shiftRight, TetriminoState, undrop } from '../../gameplay';
 import { Tetrimino } from '~/tetrimino';
@@ -10,14 +12,33 @@ export function zicDetector(
 ): ZicData | undefined {
   if (!state) return undefined;
 
-  // the provided board already includes the fixed piece
-  // so we need to remove the fixed piece from the board
-  // or any test will fail
   const fixedPieceMinos = calculateMatrix(
     state.shape,
     state.position,
     state.plane,
   );
+
+  const checkMino = checkMinoOn(board);
+
+  const isPerfectFit = fixedPieceMinos.every((fp) => {
+    const over = { x: fp.x, y: fp.y - 1, z: fp.z };
+    const under = { x: fp.x, y: fp.y + 1, z: fp.z };
+    const left = match(state.plane)
+      .with('x', () => ({ x: fp.x, y: fp.y, z: fp.z - 1 }))
+      .with('z', () => ({ x: fp.x - 1, y: fp.y, z: fp.z }))
+      .exhaustive();
+    const right = match(state.plane)
+      .with('x', () => ({ x: fp.x, y: fp.y, z: fp.z + 1 }))
+      .with('z', () => ({ x: fp.x + 1, y: fp.y, z: fp.z }))
+      .exhaustive();
+    console.log([over, under, left, right].map(checkMino));
+    return [over, under, left, right].map(checkMino).every(Boolean);
+  });
+  if (isPerfectFit) return { kind: 'full' };
+
+  // the provided board already includes the fixed piece
+  // so we need to remove the fixed piece from the board
+  // or any test will fail
   const prevBoard = board.filter(
     (b) =>
       !fixedPieceMinos.some(
@@ -25,16 +46,31 @@ export function zicDetector(
       ),
   );
 
-  // todo: check if the piece is perfectly fitted into a gap
-  const isPerfectFit = false;
-  if (isPerfectFit) return { kind: 'full' };
-
   const testMove = testMoveOn(state, prevBoard);
   const canMoveOnPlane = [undrop, shiftLeft, shiftRight]
     .map(testMove)
     .some(Boolean);
   return canMoveOnPlane ? undefined : { kind: 'mini' };
 }
+
+const checkMinoOn =
+  (
+    board: {
+      x: number;
+      y: number;
+      z: number;
+    }[],
+  ) =>
+  (mino: { x: number; y: number; z: number }) => {
+    return (
+      mino.y >= ROWS + VANISH_ZONE_ROWS ||
+      mino.x < 0 ||
+      mino.x >= COLS ||
+      mino.z < 0 ||
+      mino.z >= COLS ||
+      board.some((b) => b.x === mino.x && b.y === mino.y && b.z === mino.z)
+    );
+  };
 
 const testMoveOn =
   (
